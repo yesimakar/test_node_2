291f10161c26ea5eea0b0583777d74944178a567
NUCAL <yesim.akar@nurd.com>
2023-05-12 14:50:51 +0300

    sdsdsdsd

---
+++ src/components/pages/Screen1.vue
@@ -0,0 +1,120 @@
+<template>
+  <DropZone ncBgType="bgColor" backgroundImage="" id="06f60d9b-d2dc-4abd-987b-f3dc9cf50bbb"
+    v-bind:style="{'width':'100%','height':'auto'}">
+    <WButton tag="button" href="" v-bind:pill="false" size="md" text="Button" type="button" v-bind:block="false"
+      color="#FFFFFF" theme="theme--light01" v-bind:squared="false" variant="primary" ncBgType="bgColor"
+      v-bind:isEnabled="true" background="#007bff" linkTarget="_top" ncLinkType="link" borderColor="#007bff"
+      ncTypography="body1" v-bind:resetTrigger="false" ncTooltipText="" verticalWidth="60px"
+      v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+      verticalHeight="60px" horizontalWidth="72.4479px" nucalScaleWidth="" toForRouterLink="to"
+      horizontalHeight="37.5347px" ncClickFontColor="#FFFFFF" ncFocusFontColor="#FFFFFF" ncHoverFontColor="#FFFFFF"
+      nucalScaleHeight="" ncClickBorderColor="#007BFF" ncDisableFontColor="#FFFFFF" ncFocusBorderColor="#007BFF"
+      ncHoverBorderColor="#007BFF" ncDisableBorderColor="transparent" ncClickBackgroundColor="#007BFF"
+      ncFocusBackgroundColor="#007BFF" ncHoverBackgroundColor="#007BFF" ncDisableBackgroundColor="#969696"
+      position="relative" v-bind:snapX="false" v-bind:snapY="false" id="c91d761e-4b5a-41d3-a25e-a61c3e456f2a"
+      v-bind:style="{'width':'100%','filter':'none','height':'100%','tabIndex':'0px','boxShadow':'','fontStyle':'normal','marginTop':'','textAlign':'center','fontFamily':'OpenSans-Bold,Open Sans','fontWeight':'normal','lineHeight':'1.4','marginLeft':'','paddingTop':'','textShadow':'','visibility':'visible','borderStyle':'solid','marginRight':'','paddingLeft':'','marginBottom':'','mixBlendMode':'normal','paddingRight':'','paddingBottom':'','backdropFilter':'none','borderTopWidth':'1px','textDecoration':'none','borderLeftWidth':'1px','borderRightWidth':'1px','horizontalHeight':'37.5347px','borderBottomWidth':'1px','borderTopLeftRadius':'4px','borderTopRightRadius':'4px','borderBottomLeftRadius':'4px','borderBottomRightRadius':'4px','zIndex':2}"/>
+    <WDangerButton tag="button" href="" v-bind:pill="false" size="md" text="Danger" type="button" v-bind:block="false"
+      color="#FFFFFF" theme="theme--light01" v-bind:squared="false" variant="danger" v-bind:isEnabled="true"
+      background="#dc3545" linkTarget="_top" ncLinkType="link" borderColor="#dc3545" ncTypography="body1"
+      v-bind:resetTrigger="false" ncTooltipText="" verticalWidth="60px"
+      v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+      verticalHeight="60px" horizontalWidth="72.4479px" toForRouterLink="to" horizontalHeight="37.5347px"
+      ncClickFontColor="#FFFFFF" ncFocusFontColor="#FFFFFF" ncHoverFontColor="#FFFFFF" ncClickBorderColor="#dc3545"
+      ncDisableFontColor="#FFFFFF" ncFocusBorderColor="#dc3545" ncHoverBorderColor="#dc3545"
+      ncDisableBorderColor="transparent" ncClickBackgroundColor="#dc3545" ncFocusBackgroundColor="#dc3545"
+      ncHoverBackgroundColor="#dc3545" ncDisableBackgroundColor="#969696" position="relative" v-bind:snapX="false"
+      v-bind:snapY="false" id="59a56f7b-06d9-429a-a4df-e612385ef03d"
+      v-bind:style="{'width':'100%','height':'100%','tabIndex':'0px','boxShadow':'','fontStyle':'normal','marginTop':'0px','textAlign':'center','fontFamily':'OpenSans-Bold,Open Sans','fontWeight':'bold','lineHeight':'1.4','marginLeft':'0px','paddingTop':'','textShadow':'','visibility':'visible','borderStyle':'solid','marginRight':'0px','paddingLeft':'','marginBottom':'0px','paddingRight':'','paddingBottom':'','borderTopWidth':'1px','textDecoration':'none','borderLeftWidth':'1px','borderRightWidth':'1px','horizontalHeight':'37.5347px','borderBottomWidth':'1px','borderTopLeftRadius':'4px','borderTopRightRadius':'4px','borderBottomLeftRadius':'4px','borderBottomRightRadius':'4px','zIndex':3}"/>
+  </DropZone>
+</template>
+<script>
+export default {
+  name: 'Screen1',
+  data() {
+    return {};
+  },
+  props: {},
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped>@media (min-width: 1024.05px){
+[id='06f60d9b-d2dc-4abd-987b-f3dc9cf50bbb'] {
+             display: grid; 
+             grid-template-columns: 18.984375rem 20.234375rem 5.078125rem 37.03125rem 18.671875rem;
+             grid-template-rows: minmax(4.84375rem, max-content) minmax(12.34375rem, max-content) minmax(7.1875rem, max-content) minmax(11.015625rem, max-content) minmax(22.8125rem, max-content);
+          }
+[id='c91d761e-4b5a-41d3-a25e-a61c3e456f2a']{
+          grid-area: 2/2/3/3;
+          font-size: Max(Min(12rem, 12px),0.9375rem);
+          }
+[id='59a56f7b-06d9-429a-a4df-e612385ef03d']{
+          grid-area: 4/4/5/5;
+          font-size: Max(Min(12rem, 12px),0.9375rem);
+          }
+}
+@media (min-width: 768.05px) and (max-width: 1024px){
+[id='06f60d9b-d2dc-4abd-987b-f3dc9cf50bbb'] {
+             display: grid; 
+             grid-template-columns: 26.85546875rem 10.498046875rem 25.29296875rem 10.498046875rem 26.85546875rem;
+             grid-template-rows: minmax(6.0546875rem, max-content) minmax(15.4296875rem, max-content) minmax(8.984375rem, max-content) minmax(13.76953125rem, max-content) minmax(3.125rem, max-content);
+          }
+[id='c91d761e-4b5a-41d3-a25e-a61c3e456f2a']{
+          grid-area: 2/3/3/4;
+          font-size: Max(Min(12rem, 12px),1.171875rem);
+          }
+[id='59a56f7b-06d9-429a-a4df-e612385ef03d']{
+          grid-area: 4/2/5/5;
+          font-size: Max(Min(12rem, 12px),1.171875rem);
+          }
+}
+@media (min-width: 480.05px) and (max-width: 768px){
+[id='06f60d9b-d2dc-4abd-987b-f3dc9cf50bbb'] {
+             display: grid; 
+             grid-template-columns: 19.140624999999996rem 13.997395833333332rem 33.723958333333336rem 13.997395833333336rem 19.140624999999996rem;
+             grid-template-rows: minmax(8.072916666666666rem, max-content) minmax(20.572916666666668rem, max-content) minmax(11.979166666666666rem, max-content) minmax(18.359375rem, max-content) minmax(4.166666667rem, max-content);
+          }
+[id='c91d761e-4b5a-41d3-a25e-a61c3e456f2a']{
+          grid-area: 2/3/3/4;
+          font-size: Max(Min(12rem, 12px),1.5625rem);
+          }
+[id='59a56f7b-06d9-429a-a4df-e612385ef03d']{
+          grid-area: 4/2/5/5;
+          font-size: Max(Min(12rem, 12px),1.5625rem);
+          }
+}
+@media (min-width: 375.05px) and (max-width: 480px){
+[id='06f60d9b-d2dc-4abd-987b-f3dc9cf50bbb'] {
+             display: grid; 
+             grid-template-columns: 5rem 18.020833333333332rem 53.958333333333336rem 18.020833333333336rem 5rem;
+             grid-template-rows: minmax(12.916666666666666rem, max-content) minmax(32.916666666666664rem, max-content) minmax(19.166666666666668rem, max-content) minmax(26.772151898734176rem, max-content) minmax(5rem, max-content);
+          }
+[id='c91d761e-4b5a-41d3-a25e-a61c3e456f2a']{
+          grid-area: 2/3/3/4;
+          font-size: Max(Min(12rem, 12px),2.5rem);
+          }
+[id='59a56f7b-06d9-429a-a4df-e612385ef03d']{
+          grid-area: 4/2/5/5;
+          font-size: Max(Min(12rem, 12px),2.2784810126582276rem);
+          }
+}
+@media (min-width: 0px) and (max-width: 375px){
+[id='06f60d9b-d2dc-4abd-987b-f3dc9cf50bbb'] {
+             display: grid; 
+             grid-template-columns: 6.4rem 0 87.2rem 0 6.4rem;
+             grid-template-rows: minmax(16.533333333333335rem, max-content) minmax(53.1953667953668rem, max-content) minmax(24.533333333333335rem, max-content) minmax(25.939240506329117rem, max-content) minmax(6.4rem, max-content);
+          }
+[id='c91d761e-4b5a-41d3-a25e-a61c3e456f2a']{
+          grid-area: 2/2/3/5;
+          font-size: Max(Min(12rem, 12px),4.040154440154441rem);
+          }
+[id='59a56f7b-06d9-429a-a4df-e612385ef03d']{
+          grid-area: 4/3/5/4;
+          font-size: Max(Min(12rem, 12px),2.207594936708861rem);
+          }
+}
+</style>
